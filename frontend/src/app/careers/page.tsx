3d71d4a75464ca4ac79471ba7620984a821a7734
winsonallan <winsonallanw@gmail.com>
2025-10-20 14:45:43 +0700

    Bug margin bottom image in career page fixed

--- frontend/src/app/careers/page.tsx
+++ frontend/src/app/careers/page.tsx
@@ -76,7 +76,6 @@ export default function Careers() {
 									style={{
 										objectFit: 'cover',
 										objectPosition: 'top',
-										marginBottom: '.5rem',
 									}}
 								/>
 							</div>
